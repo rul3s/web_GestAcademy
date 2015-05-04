6564ac390229e95a0ab2b40e41664589fc1676c2
Raul <valls.raul@gmail.com>
2015-05-05 01:41:20 +0200

    Added XML and JSON export for each model

--- AcademyApp/urls.py
+++ AcademyApp/urls.py
@@ -6,12 +6,23 @@ urlpatterns = [
     # regularExpr, view, keyword args, name
     # ex: /AcademyApp/
     url(r'^$', views.index, name='index'),
+
     # ex: /AcademyApp/get/list
     url(r'^students/get/list$', views.students, name='getStudents'),
     # ex: /AcademyApp/get/5
     url(r'^students/get/(?P<student_id>[0-9]+)', views.student, name='getStudent'),
+    # ex: /AcademyApp/get/list.json
+    url(r'^students/get/list.json$', views.studentsjson, name='getStudentsJson'),
+    # ex: /AcademyApp/get/list.xml
+    url(r'^students/get/list.xml$', views.studentsxml, name='getStudentsXML'),
+
     url(r'^teachers/get/list$', views.teachers, name='getTeachers'),
     url(r'^teachers/get/(?P<teacher_id>[0-9]+)', views.teacher, name='getTeacher'),
+    url(r'^teachers/get/list.json$', views.teachersjson, name='getTeachersJson'),
+    url(r'^teachers/get/list.xml$', views.teacherssxml, name='getTeachersXML'),
+
     url(r'^academies/get/list$', views.academies, name='getAcademies'),
-    url(r'^academies/get/(?P<academy_id>[0-9]+)', views.academy, name='getAcademie'),
+    url(r'^academies/get/(?P<academy_id>[0-9]+)', views.academy, name='getAcademy'),
+    url(r'^academies/get/list.json$', views.academiesjson, name='getAcademiesJson'),
+    url(r'^academies/get/list.xml$', views.academiesxml, name='getAcademiesXML'),
 ]
--- AcademyApp/views.py
+++ AcademyApp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from models import *
 from django.template import RequestContext, loader
+from django.core import serializers
 
 # Create your views here.
 
 
 def index(request):
-    output = "*************LIST OF DIFERENT OBJECTS***************<br>"
+    output = "*************DATABASE GENERAL INFO***************<br>"
 
     output += "<br><br>Academies:<br>"
     academies_list = Academy.objects.order_by('registered')
@@ -24,42 +25,77 @@ def index(request):
 
 
 def students(request):
-    latest_students_list = Student.objects.order_by('registered')
+    students_list = Student.objects.order_by('registered')
     template = loader.get_template('AcademyApp/students.html')
     context = RequestContext(request, {
-        'latest_students_list': latest_students_list,
+        'students_list': students_list,
     })
 
     return HttpResponse(template.render(context))
 
 
 def student(request, student_id):
-    return HttpResponse("Hello world. You're looking for student %s" % student_id)
+    try:
+        std = Student.objects.get(pk=student_id)
+        context = {'std': std}
+    except Student.DoesNotExist:
+        raise Http404("Student does not exist")
+    return render(request, 'AcademyApp/student.html', context)
+    # return HttpResponse("You're looking for student %s" % student_id)
+    # return HttpResponse("Name = " +std.name)
+
+
+def studentsjson(request):
+    return HttpResponse(serializers.serialize('json', Student.objects.all()))
+
+
+def studentsxml(request):
+    return HttpResponse(serializers.serialize('xml', Student.objects.all()))
 
 
 def teachers(request):
     teachers_list = Teacher.objects.order_by('registered')
-    template = loader.get_template('AcademyApp/teachers.html')
-    context = RequestContext(request, {
-        'teachers_list': teachers_list,
-    })
+    context = {'teachers_list': teachers_list}
 
-    return HttpResponse(template.render(context))
+    return render(request, 'AcademyApp/teachers.html', context)
 
 
 def teacher(request, teacher_id):
-    return HttpResponse("Hello world. You're looking for teacher %s " % teacher_id)
+    try:
+        tchr = Teacher.objects.get(pk=teacher_id)
+        context = {'tchr': tchr}
+    except Teacher.DoesNotExist:
+        raise Http404("Teacher does not exist")
+    return render(request, 'AcademyApp/teacher.html', context)
+
+
+def teachersjson(request):
+    return HttpResponse(serializers.serialize('json', Teacher.objects.all()))
+
+
+def teacherssxml(request):
+    return HttpResponse(serializers.serialize('xml', Teacher.objects.all()))
 
 
 def academies(request):
     academies_list = Academy.objects.order_by('registered')
-    template = loader.get_template('AcademyApp/academies.html')
-    context = RequestContext(request, {
-        'academies_list': academies_list,
-    })
+    context = {'academies_list': academies_list}
 
-    return HttpResponse(template.render(context))
+    return render(request, 'AcademyApp/academies.html', context)
 
 
 def academy(request, academy_id):
-    return HttpResponse("Hello world. You're looking for academy %s" % academy_id)
+    try:
+        acdmy = Academy.objects.get(pk=academy_id)
+        context = {'acdmy': acdmy}
+    except Academy.DoesNotExist:
+        raise Http404("Academy does not exist")
+    return render(request, 'AcademyApp/academy.html', context)
+
+
+def academiesjson(request):
+    return HttpResponse(serializers.serialize('json', Academy.objects.all()))
+
+
+def academiesxml(request):
+    return HttpResponse(serializers.serialize('xml', Academy.objects.all()))

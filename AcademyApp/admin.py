e0f98daf9b5111c9895bfd2b6d3489da4e024916
Raul <valls.raul@gmail.com>
2015-05-04 15:49:45 +0200

    All models working Testing personalized Create Student

--- AcademyApp/admin.py
+++ AcademyApp/admin.py
@@ -3,7 +3,18 @@ from .models import *
 
 # Register your models here.
 
+'''
+class StudentAdmin(admin.ModelAdmin):
+    fields = ['name', 'direction']
+'''
+
 admin.site.register(Academy)
 admin.site.register(Teacher)
+admin.site.register(Student)
+# admin.site.register(Student, StudentAdmin)
+admin.site.register(TeacherStudents)
+
+
+
 
 
--- AcademyApp/models.py
+++ AcademyApp/models.py
@@ -1,31 +1,54 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
-class Teacher(models.Model):
-    name = models.CharField(max_length=50)
+
+class Student(models.Model):
+    name = models.CharField(max_length=60)
     direction = models.CharField(max_length=150)
     city = models.CharField(max_length=50)
     telephone = models.CharField(max_length=9)
     email = models.EmailField()
-    #students = models.ForeignKey(Student)
+    academy = models.ForeignKey('Academy', blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=60)
+    direction = models.CharField(max_length=150)
+    city = models.CharField(max_length=50)
+    telephone = models.CharField(max_length=9)
+    email = models.EmailField()
+    registered = models.DateTimeField(default=datetime.datetime.now())
+
+    def __str__(self):
+        return self.name
+
+    academy = models.ForeignKey('Academy', blank=True, null=True)
 
 
 class Academy(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=60)
     direction = models.CharField(max_length=150)
     city = models.CharField(max_length=50)
-    teacher = models.ForeignKey(Teacher, blank=True, null=True)
-    #students = models.ForeignKey(Student)
+    registered = models.DateTimeField(default=datetime.datetime.now())
+
+    def __str__(self):
+        return self.name
 
 
-'''
-class Student(models.Model):
-    name = models.CharField()
-    direction = models.CharField()
-    city = models.CharField()
-    telephone = models.CharField(max_length=9)
-    email = models.EmailField()
-    teachers = models.ForeignKey(Teacher)
-'''
+class TeacherStudents(models.Model):
+    registered = models.DateTimeField(default=datetime.datetime.now())
+    teacher = models.ForeignKey('Teacher')
+    student = models.ForeignKey('Student')
+
+    def __str__(self):
+        return str(self.registered) + " " + self.teacher.name + " " + self.student.name
+
+
+
+
+

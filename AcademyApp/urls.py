33aa4573b3e4378bd7e36d6eee4a6ea58e39e5fe
Raul <valls.raul@gmail.com>
2015-05-05 00:19:38 +0200

    Views returning object lists

--- AcademyApp/urls.py
+++ AcademyApp/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     # regularExpr, view, keyword args, name
     # ex: /AcademyApp/
     url(r'^$', views.index, name='index'),
-    # ex: /AcademyApp/students
-    url('students', views.liststudents, name='liststudents'),
-    url('teachers', views.listteachers, name='listteachers'),
-    url('academies', views.listacademies, name='listacademies'),
+    # ex: /AcademyApp/get/list
+    url(r'^students/get/list$', views.students, name='getStudents'),
+    # ex: /AcademyApp/get/5
+    url(r'^students/get/(?P<student_id>[0-9]+)', views.student, name='getStudent'),
+    url(r'^teachers/get/list$', views.teachers, name='getTeachers'),
+    url(r'^teachers/get/(?P<teacher_id>[0-9]+)', views.teacher, name='getTeacher'),
+    url(r'^academies/get/list$', views.academies, name='getAcademies'),
+    url(r'^academies/get/(?P<academy_id>[0-9]+)', views.academy, name='getAcademie'),
 ]
--- AcademyApp/views.py
+++ AcademyApp/views.py
@@ -1,21 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import *
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello world. You're at the Academy index.")
+    output = "*************LIST OF DIFERENT OBJECTS***************<br>"
+
+    output += "<br><br>Academies:<br>"
+    academies_list = Academy.objects.order_by('registered')
+    output += ', '.join([a.name for a in academies_list])
+
+    output += "<br><br>Teachers:<br>"
+    teachers_list = Teacher.objects.order_by('registered')
+    output += ', '.join([t.name for t in teachers_list])
+
+    output += "<br><br>Students:<br>"
+    students_list = Student.objects.order_by('registered')
+    output += ', '.join([s.name for s in students_list])
+    return HttpResponse(output)
 
 
-def liststudents(request):
-    return HttpResponse("Hello world. You're at the Students list.")
+def students(request):
+    output = "*************Students LIST*************<br>"
+    students_list = Student.objects.order_by('registered')
+    output += ', '.join([s.name for s in students_list])
+    return HttpResponse(output)
 
 
-def listteachers(request):
-    return HttpResponse("Hello world. You're at the Teachers list.")
+def student(request, student_id):
+    return HttpResponse("Hello world. You're looking for student %s" % student_id)
 
 
-def listacademies(request):
-    return HttpResponse("Hello world. You're at the Academies list.")
+def teachers(request):
+    output = "*************Teachers LIST*************<br>"
+    teachers_list = Teacher.objects.order_by('registered')
+    output += ', '.join([t.name for t in teachers_list])
+    return HttpResponse(output)
 
+
+def teacher(request, teacher_id):
+    return HttpResponse("Hello world. You're looking for teacher %s " % teacher_id)
+
+
+def academies(request):
+    output = "*************Academies LIST*************<br>"
+    academies_list = Academy.objects.order_by('registered')
+    output += ', '.join([a.name for a in academies_list])
+    return HttpResponse(output)
+
+
+def academy(request, academy_id):
+    return HttpResponse("Hello world. You're looking for academy %s" % academy_id)

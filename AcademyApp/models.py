f6e824dc881ee0d5641fe26b0d38d856052a99b3
Raul <valls.raul@gmail.com>
2015-05-04 13:15:19 +0200

    App created Added firsts models

--- AcademyApp/models.py
+++ AcademyApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    direction = models.CharField(max_length=150)
+    city = models.CharField(max_length=50)
+    telephone = models.CharField(max_length=9)
+    email = models.EmailField()
+    #students = models.ForeignKey(Student)
+
+
+
+class Academy(models.Model):
+    name = models.CharField(max_length=50)
+    direction = models.CharField(max_length=150)
+    city = models.CharField(max_length=50)
+    teacher = models.ForeignKey(Teacher)
+    #students = models.ForeignKey(Student)
+
+
+'''
+class Student(models.Model):
+    name = models.CharField()
+    direction = models.CharField()
+    city = models.CharField()
+    telephone = models.CharField(max_length=9)
+    email = models.EmailField()
+    teachers = models.ForeignKey(Teacher)
+'''
